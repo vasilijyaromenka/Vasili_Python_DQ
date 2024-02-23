1a910f6c0d416eed5dd6c7e6b8b04c7bf924226b
vasili_yaromenka <vasili_yaromenka@epam.com>
2024-02-23 18:12:40 +0200

    final

--- hw_03_vasili_yaromenka.py
+++ hw_03_vasili_yaromenka.py
@@ -10,19 +10,25 @@ txt = """homEwork:
 	last iz TO calculate nuMber OF Whitespace characteRS in this Text. caREFULL, not only Spaces, but ALL whitespaces. I got 87.
 """
 
-# normalize it from letter CASEs point oF View
 
 # declare a RE to capture 1st word of the sentence
 # ([:.!?]\s*): create a RE to capture the 1 st word after [:.!?],regardless the number of with spaces (\s*) after the symbol
 # (\S+) : captures a group of 1 or more non-whitespace characters
 first_word_re = r'([:.!?]\s*)(\S+)'
 
-# a RE to captures "iz" when it is a separate word and is not quoted
+# a RE to capture the last word of a sentence
+last_word_re = r'\b\w+\b(?=[.!?])'
+
+
+# a RE to capture "iz" when it is a separate word and is not quoted
 iz_re = re.compile(r'(?<!“)\biz\b(?!”)', re.IGNORECASE)
 
 # a RE to captures aby single white space
 whitespace_re = r'\s'
 
+
+
+# 1) Implementation of the scenario with replacing Fix"iz" with Fix "iz" '
 print('1) Implementation of the scenario with replacing Fix"iz" with Fix "iz" ')
 
 # function to capitalize all the words in the match, but as there is only one in first_word_re, it fits
@@ -46,13 +52,14 @@ txt_fix_1 = re.sub(first_word_re, capitalize_word, txt_fix_1)
 # count white spaced
 white_space_num = len(re.findall(whitespace_re, txt_fix_1))
 
-print(f"1st fixed version:\n\n{txt_fix_1}")
+print(f"1st normalized version:\n\n{txt_fix_1}")
 print(f"There are {white_space_num} white spaces in the text")
 
 
 
-print('\n' + '='*100 + '\n\n2) Implementation of the scenario with keeping Fix"iz" as is ')
 
+# 2) Implementation of the scenario with keeping Fix"iz" as is '
+print('\n' + '='*100 + '\n\n2) Implementation of the scenario with keeping Fix"iz" as is ')
 
 def capitalize_word_2 (match):
     start_spaces = match.group(1)
@@ -75,3 +82,18 @@ white_space_num_2 = len(re.findall(whitespace_re, txt_fix_2))
 
 print(f"2nd fixed version:\n\n{txt_fix_2}")
 print(f"There are {white_space_num_2} white spaces in the text")
+
+
+# 3) Creating a sentence from last words and adding to the text
+print('\n' + '='*100 + '\n\n3) Creating a sentence from last words \n')
+
+last_words_lst = re.findall(last_word_re, txt_fix_2)
+last_words_sent = ' '.join(last_words_lst).capitalize() + '.'
+final_str = txt_fix_2 + "\n\t" + last_words_sent
+
+print(final_str)
+
+
+
+
+
